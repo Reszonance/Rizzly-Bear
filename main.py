d2521507ff6f36e63ef411150a47e00f1fc5cd88
Johnny Ling <johnnyling77@gmail.com>
2023-03-10 05:38:57 -0500

    Fixed database not working and cleaned up code

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ mongo_client = pymongo.MongoClient(os.getenv("MONGO_URI"))
 mongo_db = mongo_client['rizzults']
 
 # Set up MongoDB collection
-conversation_history_collection = mongo_db['convseration-history']
+convo_history_collection = mongo_db['conversation-history']
 
 # Set up Discord bot credentials
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
-# GUILD = os.getenv("DISCORD_GUILD_ID")
 
 # Set up Discord client
 intents = discord.Intents.all()
@@ -29,9 +28,7 @@ intents.messages = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Define initial prompt
-question = os.getenv("QUESTION")
-initial_prompt = os.getenv("INITIAL_PROMPT") + question
+convo_history = None
 
 
 async def generate_pickup_line(prompt):
@@ -51,65 +48,53 @@ async def generate_pickup_line(prompt):
 async def pickup_command(ctx, *args):
     user_id = str(ctx.author.id)
 
-    def check(message):
-        return message.author == ctx.author and message.channel == ctx.channel
+    # Retrieve conversation history from the database
+    convo_history = convo_history_collection.find_one({"user_id": user_id})
 
     # Check if there are any arguments passed
     if args:
         # Get the topic from the argument
         topic = ' '.join(args)
         # Create a new conversation prompt based on the topic
-        prompt = os.getenv("INITIAL_PROMPT") + f"Can you give me a pickup line about {topic}?\n"
+        prompt = f"Can you give me a unique and witty pickup line about {topic}?"
     else:
         # Get the most recent conversation prompt
-        if user_id not in conversation_history:
-            conversation_history[user_id] = {
-                'prompts': [initial_prompt],
-                'responses': ['']
-            }
+        if convo_history_collection.count_documents({"user_id": user_id}) == 0:
+            convo_history_collection.insert_one({
+                "user_id": user_id,
+                "prompts": [""],
+                "responses": ['']
+            })
 
-        prompt = conversation_history[user_id]['prompts'][-1]
+        prompt = convo_history['prompts'][-1]
 
-        # Wait for user to respond
-        await ctx.send("Type 'continue' to continue the conversation or 'new' to start a new one.")
-        response = await bot.wait_for('message', check=check)
-
-        # Handle "new" command
-        while response.content.lower() not in ['continue', 'new']:
-            await ctx.send("Invalid response. Type 'continue' to continue the conversation or 'new' to start a new one.")
-            response = await bot.wait_for('message', check=check)
-
-        if response.content.lower() == 'new':
-            await ctx.send("Okay, let's start a new conversation!")
-            conversation_history[user_id] = {
-                'prompts': [initial_prompt],
-                'responses': ['']
-            }
-            prompt = initial_prompt
-            print('new')
-        else:
-            # Get the most recent conversation prompt
-            new_prompt = conversation_history[user_id]['responses'][-1]
-            prompt = new_prompt + question
-            print('continuing')
-
-    print("\n" + prompt)
-    prompt = prompt.replace('\n', '').replace(',', '').replace('.', '').replace('!', '').replace('?', '')
     # Generate response using OpenAI API
     response = await generate_pickup_line(prompt)
 
-    # Update conversation history
-    conversation_history[user_id]['prompts'].append(prompt)
-    conversation_history[user_id]['responses'].append(response)
-
-    # Store conversation history in MongoDB
-    conversation_history_collection.update_one(
+    # Update conversation history in the database
+    convo_history_collection.update_one(
         {"user_id": user_id},
-        {"$set": {"conversation_history": conversation_history[user_id]}},
+        {
+            "$push": {"prompts": prompt, "responses": response},
+            "$set": {"conversation-history": convo_history},
+        },
         upsert=True,
     )
 
     # Send response to user
     await ctx.send(response)
+
+# Maybe add this to the backend so users wont be able to use this
+@bot.command(name='reset')
+async def reset_collection(ctx):
+    user_id = str(ctx.author.id)
+
+    # Check if user has a conversation history collection
+    if convo_history_collection.count_documents({"user_id": user_id}) == 0:
+        await ctx.send("You don't have any history of conversation.")
+    else:
+        # Delete user's conversation history collection
+        convo_history_collection.delete_one({"user_id": user_id})
+        await ctx.send("Your conversation history has been reset.")
 
 bot.run(TOKEN)
